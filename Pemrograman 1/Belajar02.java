811f97193606b414cee0c50908337e2696708910
Mohammad Rofi Rahman <rofirahman2506@gmail.com>
2017-08-30 15:32:05 +0700

    Created by Coding Ground Client

---
+++ Pemrograman 1/Belajar02.java
@@ -0,0 +1,22 @@
+public class Belajar02 {
+    public static void main(String[] args) {
+        System.out.println("\nProgram Penghitungan Berat Badan (Java8)");
+        
+        // Menggunakan Tipe Data "double" untuk pecahan dan nama variable "berat"
+        double berat;
+        // Menentukan isi variable
+        berat = 41.2;
+        // Menampilkan isi variable pertama
+        System.out.println("Berat badan awal = " + berat);
+        
+        // Menentukan isi variable kedua ditambah variable pertama
+        berat = berat + 2;
+        // Menampilkan hasil penjumlahan variable kedua
+        System.out.println("Berat badan setelah makan = " + berat);
+        
+        // Menentukan isi variable ketiga dikurang variable kedua
+        berat = berat - 1;
+        // Menampilkan hasil pengurangan variable ketiga
+        System.out.println("Berat badan tanggal tua = " + berat + "\n");
+    }
+}
