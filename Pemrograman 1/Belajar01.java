e20f98a670281d4a5442bf66e973ee1b206f4a48
Mohammad Rofi Rahman <rofirahman2506@gmail.com>
2017-08-30 15:31:20 +0700

    Created by Coding Ground Client

---
+++ Pemrograman 1/Belajar01.java
@@ -0,0 +1,11 @@
+public class Belajar01 {
+    public static void main(String[] args) {
+        System.out.println("\nProgram Menampilkan Nama dan NIM (Java8)");
+        
+        // Menampilkan Nama
+        System.out.println("Nama : Mohammad Rofi Rahman");
+        
+        // Menampilkan NIM
+        System.out.println("Nim :" + 17220029 + "\n");
+    }
+}
